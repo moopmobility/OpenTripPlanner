be986e2ede8e3083564584424cc45c72554950f3
Zsombor Welker <zsombor.welker@realcity.io>
2023-09-08 09:52:17 +0200

    Use maxFlexTripDuration in direct flex path calculator

--- src/ext/java/org/opentripplanner/ext/flex/flexpathcalculator/DirectFlexPathCalculator.java
+++ src/ext/java/org/opentripplanner/ext/flex/flexpathcalculator/DirectFlexPathCalculator.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.ext.flex.flexpathcalculator;
 
+import java.time.Duration;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
 import org.opentripplanner.framework.geometry.GeometryUtils;
@@ -15,22 +16,26 @@ public class DirectFlexPathCalculator implements FlexPathCalculator {
   private static final int DIRECT_EXTRA_TIME = 5 * 60;
 
   private final double flexSpeed;
+  private final Duration maxFlexTripDuration;
 
   public DirectFlexPathCalculator(FlexConfig config) {
     this.flexSpeed = config.directFlexPathSpeed();
+    this.maxFlexTripDuration = config.maxFlexTripDuration();
   }
 
   @Override
   public FlexPath calculateFlexPath(Vertex fromv, Vertex tov, int fromStopIndex, int toStopIndex) {
     double distance = SphericalDistanceLibrary.distance(fromv.getCoordinate(), tov.getCoordinate());
+    double durationSeconds = (distance / flexSpeed) + DIRECT_EXTRA_TIME;
+
+    if (maxFlexTripDuration.toSeconds() < durationSeconds) {
+      return null;
+    }
+
     LineString geometry = GeometryUtils
       .getGeometryFactory()
       .createLineString(new Coordinate[] { fromv.getCoordinate(), tov.getCoordinate() });
 
-    return new FlexPath(
-      (int) distance,
-      (int) (distance / flexSpeed) + DIRECT_EXTRA_TIME,
-      () -> geometry
-    );
+    return new FlexPath((int) distance, (int) durationSeconds, () -> geometry);
   }
 }
